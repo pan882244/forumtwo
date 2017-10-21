96102cca53f93564292f5977aa0ea407f9aeed01
zss <123@qq.com>
2017-10-21 16:41:54 +0800

    分支用于修改

---
+++ forum-dao/src/test/java/daotest/UserDaoTest.java
@@ -0,0 +1,26 @@
+package daotest;
+
+import allDao.UsersDao;
+import entity.UsersEntity;
+import org.junit.Test;
+
+public class UserDaoTest {
+    UsersDao usersDao = new UsersDao();
+    @Test
+    public void zhuce() {
+        UsersEntity usersEntity = new UsersEntity();
+        usersEntity.setAccount("qbb");
+        usersEntity.setPassword("123");
+        usersEntity.setEmail("123@qq.com");
+
+        usersDao.insert(usersEntity);
+    }
+
+    @Test
+    public void denglu() {
+
+        UsersEntity list = usersDao.login("zss","123");
+
+        System.out.println(list.getAccount());
+    }
+}
